91fb538add866ba7c0d98b7babcf26e40329a4f0
Sila-duha <43171238+Sila-duha@users.noreply.github.com>
2018-09-12 17:36:25 +0300

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Параметры MySQL: Эту информацию можно получить у вашего хостинг-провайдера ** //
 /** Имя базы данных для WordPress */
-define('DB_NAME', 'Sila-duha.github.io');
+define('DB_NAME', 'Sila-duha.github.ru');
 
 /** Имя пользователя MySQL */
 define('DB_USER', 'Sila-duha');
